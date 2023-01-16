e820b5867294edbbf27c20f82f41c4363102d549
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-01-17 02:47:32 +0900

    Add: TOP、ユーザ、ログイン・ログアウトのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'top#index'
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  delete 'logout' => 'user_sessions#destroy', :as => :logout
 end
