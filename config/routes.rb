c0187d39555f3388d16f134212ab2ef6df19fb96
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-01-18 01:02:08 +0900

    Add: ルーティングにユーザ登録・ログイン、ログアウトを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'top#index'
-  resources :users
+  resources :users, only: %i(new create)
 
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => "user_sessions#create"
-  delete 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: "user_sessions#create"
+  delete 'logout', to: 'user_sessions#destroy'
 end
