c7c86f781bc60c71174301a3d104821d86417dda
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-02-01 07:24:07 +0900

    Add: smile_priceモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_16_165708) do
+ActiveRecord::Schema.define(version: 2023_01_31_102902) do
+
+  create_table "smile_prices", force: :cascade do |t|
+    t.integer "price"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
