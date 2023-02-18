c41eb8c83595c59b73a15ec41bb05272e2c994f1
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-02-18 15:37:55 +0900

    Add: スマイル診断機能実装

--- app/controllers/smile_prices_controller.rb
+++ app/controllers/smile_prices_controller.rb
@@ -1,17 +1,47 @@
 class SmilePricesController < ApplicationController
   skip_before_action :require_login
 
+  require 'aws-sdk-rekognition'
+
   def index; end
 
   def new; end
 
   def create
-    binding.pry
-    photo = Base64.decode64(File.expand_path(params[:image]))
-    redirect_to smile_prices_path
+    credentials = Aws::Credentials.new(
+        Rails.application.credentials[:aws][:access_key_id],
+        Rails.application.credentials[:aws][:secret_access_key]
+        # :region => 'ap-northeast-1',
+    )
+
+    # photo = Base64.decode64(File.expand_path(params[:image]))
+    client = Aws::Rekognition::Client.new region: 'ap-northeast-1', credentials: credentials
+    attrs = {
+      image: {
+        bytes: params[:image]
+      },
+      attributes: ['ALL']
+    }
+    response = client.detect_faces attrs
+    if response
+      response.face_details.each do |face_detail|
+      @smile_score = face_detail.smile.confidence
+      end
+      @smile_price = current_user.smile_prices.build(price: @smile_score)
+      if @smile_price.save
+        binding.pry
+        redirect_to smile_price_path(@smile_price), success: "OKだぜ!"
+      else
+        flash.now[:danger] = "NGだぜ!"
+        render :new
+      end
+    else
+      flash.now[:danger] = "NGだぜ!"
+      render :new
+    end
   end
 
-  def show;
-    @s = params[:id]
+  def show
+    @smile_price = SmilePrice.find(params[:id])
   end
 end
