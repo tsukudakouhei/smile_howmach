17106f3816c0a83e151b2874953020d8394a0b3e
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-03-26 15:38:25 +0900

    コミット

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  skip_before_action :require_login
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ app/javascript/packs/common.js
@@ -0,0 +1,17 @@
+"use strict";
+// console.log(1111);
+    function previewImage() {
+    console.log(1111);
+    const target = this.event.target;
+    const file = target.files[0];
+    const reader  = new FileReader();
+    reader.onloadend = function () {
+        const preview = document.querySelector("#preview")
+        if(preview) {
+            preview.src = reader.result;
+        }
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
