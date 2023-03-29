653bcbae8b7ff5950a0a1202f967cdb5fff06841
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-03-29 20:10:17 +0900

    Fix: ログインしないと診断不可に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+
+  private
+
+  def not_authenticated
+    flash[:warning] = "ログインしてください。"
+    redirect_to login_path
+  end
+
 end
