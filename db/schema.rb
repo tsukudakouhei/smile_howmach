0fe7c0a0a4b58d6278297445dc1b23ab19452864
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-04-12 01:07:22 +0900

    Add: SmilePriceモデルに公開・非公開カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_12_013207) do
+ActiveRecord::Schema.define(version: 2023_04_11_153528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2023_03_12_013207) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
+    t.boolean "is_published", default: false, null: false
     t.index ["user_id"], name: "index_smile_prices_on_user_id"
   end
 
