77e72ad34dbb432c1efbc14f2cdb8773f9f03f31
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-04-20 00:55:05 +0900

    Add: smile_price_creatorを追加

--- app/services/smile_price_creator.rb
+++ app/services/smile_price_creator.rb
@@ -1,23 +1,25 @@
 class SmilePriceCreator
-  require 'aws-sdk-rekognition'
-
   def initialize(params, current_user)
     @params = params
     @current_user = current_user
   end
 
   def create_smile_price
-    aws_client = Aws::Rekognition::Client.new(region: 'ap-northeast-1', credentials: credentials)
-    response = aws_client.detect_faces(input_image_atts)
-    if response.face_details.empty?
-      return false
-    end
+    rekognition_service = RekognitionService.new(@params[:image])
+    rekognition_data = rekognition_service.analyze_image
 
-    face_details = response.face_details
-    @smile_price, @smile_value = calculate_smile_price(face_details)
-    @smile_price_record = @current_user.smile_prices.build(price: @smile_price)
+    chatgpt_service = ChatgptService.new(rekognition_data)
+    chatgpt_service_result = chatgpt_service.analyze_data
+
+    temp_smile_price_instance = chatgpt_service_result[:smile_price]
+    smile_score = chatgpt_service_result[:smile_score]
+
+    smile_price = smile_price_score_change_price(smile_score)
+
+    @smile_price_record = @current_user.smile_prices.build(price: smile_price)
+    @smile_price_record.smile_analysis_score = temp_smile_price_instance.smile_analysis_score
     if @smile_price_record.save
-      create_smileprice_macmenus
+      create_smileprice_macmenus(@smile_price_record)
       return @smile_price_record
     else
       return false
@@ -26,41 +28,18 @@ class SmilePriceCreator
 
   private
 
-  def credentials
-    Aws::Credentials.new(
-      Rails.application.credentials[:aws][:access_key_id],
-      Rails.application.credentials[:aws][:secret_access_key]
-    )
+  def smile_price_score_change_price(smile_score)
+    smile_score * 15 
   end
 
-  def input_image_atts
-    attrs = {
-      image: {
-        bytes: @params[:image]
-      },
-      attributes: ['ALL']
-    }
-  end
-
-  def calculate_smile_price(face_details)
-    smile_price, smile_value = nil, nil
-    face_details.each do |face_detail|
-      smile_confidence = face_detail.smile.confidence
-      smile_price = (smile_confidence * 15).round(2)
-      smile_value = face_detail.smile.value
-    end
-    smile_price /= 2 if !smile_value
-    return smile_price, smile_value
-  end
-
-  def create_smileprice_macmenus
-    smile_price = @smile_price_record.price
+  def create_smileprice_macmenus(smile_price)
+    smile_price = smile_price.price
     mac_menu_price_min = MacMenu.select("price").order("price asc").first.price
     jackpot_menu = rand(1..10)
     while true
       if jackpot_menu == 1
         mac_menu = MacMenu.edamame_menu.first
-      elsif jackpot_menu == 3
+      elsif jackpot_menu == 2
         mac_menu = MacMenu.poteto_menu.sample
       else
         mac_menu = MacMenu.smileprice_and_below_menu(smile_price).random_choice.first
