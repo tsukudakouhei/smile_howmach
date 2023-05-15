021f36c4d945399dbffd89cdfc0b5535eeff9844
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-05-15 23:50:44 +0900

    add: プライボシーポリシーと利用規約追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'top#index'
+  root 'tops#index'
+  get 'terms_of_use', to: "tops#terms_of_use"
+  get 'privacy_policy', to: "tops#privacy_policy"
+  
   resources :users, only: %i(new create)
   resources :smile_prices
   get 'login', to: 'user_sessions#new'
