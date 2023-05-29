1e0905caaafcffc1c12ee01991f4b819f7928524
tsukudakouhei <tsuku.pique.job@gmail.com>
2023-05-30 07:59:09 +0900

    add: requestspecで使用するloginメソッドを記載

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,12 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+module AuthHelper
+  def sign_in(user)
+    post login_path, params: { email: user.email, password: 'password' }
+  end
+end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -61,4 +67,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include AuthHelper, type: :request
 end
